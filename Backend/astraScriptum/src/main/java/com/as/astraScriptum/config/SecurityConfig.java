8fb79e5bfc09d682a91da645528869faaef8355c
Teddy-Aimerick-TCHINDA-TAKUETE-3iL <tchindateddy@gmail.com>
2025-12-04 20:46:38 +0100

    Creation du back

---
+++ Backend/astraScriptum/src/main/java/com/as/astraScriptum/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.as.astraScriptum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()) // pour simplifier en dev
+				.authorizeHttpRequests(auth -> auth
+						// Swagger & OpenAPI en libre accès
+						.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+						// Le reste : accès libre (tu peux mettre authenticated() si tu veux sécuriser)
+						.anyRequest().permitAll())
+				.httpBasic(Customizer.withDefaults()); // auth basique
+
+		return http.build();
+	}
+}
---
+++ Backend/astraScriptum/src/test/java/com/as/astraScriptum/AstraScriptumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.astraScriptum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstraScriptumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
